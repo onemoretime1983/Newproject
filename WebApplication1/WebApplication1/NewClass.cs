bf76b3af1d55d34fa2023a868d8571cb6f432e62
onemoretime1983 <adam.smith.developer.2012@gmail.com>
2012-07-26 09:56:39 +0700

    add new class

---
+++ WebApplication1/WebApplication1/NewClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class NewClass
+    {
+        private int charNum;
+        private string nameofProject;
+        public int CharNum
+        {
+            get { return charNum; }
+            set { charNum = value;}
+        }
+        public string NameOfProject
+        {
+            get { return nameofProject;}
+            set { nameofProject = value;}
+        }
+    }
+}
